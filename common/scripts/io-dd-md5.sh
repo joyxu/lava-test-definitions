a921efe1b2d172fbcac8d02491717e8004bf4c9a
Wei Xu <xuwei5@hisilicon.com>
2015-11-20 16:59:01 +0000

    update some io auto testacses
    Signed-off-by: Wei Xu <xuwei5@hisilicon.com>

--- common/scripts/io-dd-md5.sh
+++ common/scripts/io-dd-md5.sh
@@ -3,10 +3,18 @@
 #set -e
 #set -x
 
+. include/sh-test-lib
+
+TEST="io dd consistent testing"
 
 dd if=/dev/random of=testR2 bs=4096 count=256k
 md5sum testR2
-for x in {1..10}; do cp testR2 testR2.$x; md5sum testR2.$x; done | cut -d ' ' -f 1 | uniq | wc -l
-grep "1" result.log | tail -n 1 | awk '{print " pass"}'
-lava-test-case-attach io-dd-md5 result.log
-
+for x in {1..10}; do cp testR2 testR2.$x; md5sum testR2.$x; done | tee result.log
+	index=$(cut -d ' ' -f 1 < result.log | uniq | wc -l)
+if [ "$index" = "1" ]; then
+    pass_test
+else
+    fail_test "data is not consistent"
+    return 1
+fi
+exit 0
